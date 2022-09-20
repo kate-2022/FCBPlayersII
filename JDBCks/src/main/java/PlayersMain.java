0f554d93dbb3b6fe20dc8c0a323756b1b1535d02
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-09-20 11:31:33 +0200

    works

--- JDBCks/src/main/java/PlayersMain.java
+++ JDBCks/src/main/java/PlayersMain.java
@@ -1,16 +1,9 @@
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class PlayersMain {
 
-	public static void main(String[] args) {
-		
-		InputGUI gui1 = new InputGUI();
-		
-		
+	public static void main(String[] args) throws Exception {
+						
+		 new InputGUI();
 		
 	}
 
