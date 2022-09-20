15d52d462ca928a68347aa538a758e7f3ed78796
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-09-20 11:22:50 +0200

    works

--- JDBCks/src/main/java/JDBC.java
+++ JDBCks/src/main/java/JDBC.java
@@ -3,24 +3,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-
 public class JDBC {
 	
-	String shirtNumber;   // = "";
-	String lastName;      // = "";
-	String firstName;     // = "";
-    NewWindow wind1 = new NewWindow();
+	String shirtNumber;   
+	String lastName;      
+	String firstName;     
     
 	
-	JDBC(String shirtNumber,String lastName, String firstName)throws Exception{		
-		
-		
-		this.shirtNumber = shirtNumber;
-		this.lastName = lastName;
-		this.firstName = firstName;
-		
-		new InputGUI();
-		this.jdbconnect();
+	JDBC()throws Exception{		
 		
 	}
 
@@ -30,32 +20,37 @@ public class JDBC {
 		String uname  = "root";
 		String pass = "1234";
 		
-		shirtNumber = wind1.getShirtNumberNW();
-		lastName = wind1.getLastNameNW();
-		firstName = wind1.getFirstNameNW();
-			
 		
-	//  String query = "insert into players values (" + "'" + lastName + "',"+ "'" + firstName + "'," + shirtNumber +  " )";
-		String query = "insert into players values (?,?,?)"; 
+	    String query = "insert into players values (" + "'" + lastName + "',"+ "'" + firstName + "'," + shirtNumber +  " )";
+//		String query = "insert into players values (?,?,?)"; 
 		Class.forName("com.mysql.cj.jdbc.Driver");                        // in detail: @ youtube "Class.forName" telusko
 		Connection con = DriverManager.getConnection(url, uname,pass);
 		PreparedStatement pst = con.prepareStatement(query);
+	//	ResultSet rs = pst.executeQuery(query);
+	
+	//	pst.setString(1,wind1.getLastNameNW());  // index number is  referring to number of question mark (1 = first ? in String variable query)
+	//	pst.setString(2,wind1.getFirstNameNW());
+	//	pst.setString(3,wind1.getShirtNumberNW());
+	
 		
-	   //  String lastName = textField1.getText();
-	   //  String firsName = textField2.getText();
-	
-		pst.setString(1,lastName);  // index number is  referring to number of question mark (1 = first ? in String variable query)
-		pst.setString(2,firstName);
-		pst.setString(3,shirtNumber);
-	
-				
+	 //     Statement st = con.createStatement();   // use PreparedStatement instead for inserting/ changing of values!
+
 		int count = pst.executeUpdate();    // count = number of rows that are effected                                     
+	//	ResultSet rs = pst.executeQuery(query); 
 		
 		System.out.println(count + " row/s affected");
 		
+	/*	String userData ="";
+		
+		while(rs.next()) {                                               // rs.next() is responsible to take the cursor to the next element (next row)!
+	//	String name = rs.getString("shirtNumber");                                // and rs.next() gives back a boolean value for if there is a next row or not
+		userData = rs.getString(1) + ", " + rs.getString(2)+ " : " + rs.getInt(3); 
+		System.out.println(userData);
+		}*/
 		
 		pst.close();
 		con.close();
+		
 
 
 	}
@@ -73,6 +68,9 @@ public class JDBC {
 	}
 
 }
+
+
+
 
 
 
