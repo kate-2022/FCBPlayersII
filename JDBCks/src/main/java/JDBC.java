18c823bfd537a0002b8989cd613d579f066ce7f1
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-10-10 15:17:52 +0200

    semifinal

--- JDBCks/src/main/java/JDBC.java
+++ JDBCks/src/main/java/JDBC.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
+
+
 public class JDBC {
 	
 	String shirtNumber;   
@@ -21,20 +23,37 @@ public class JDBC {
 		String pass = "1234";
 		
 		
+	//	String query = "select * from players";
+	//	String query = "insert into players values ('Goretzka', 'Leon', 8)";
+	  //	int shirtNumber = 4;
+	  //	String lastName = "de Ligt";
+	  //	String firstName = "Matthijs";
+	
+		
+	/*	shirtNumber = wind1.getShirtNumberNW();
+		lastName = wind1.getLastNameNW();
+		firstName = wind1.getFirstNameNW();*/
+			
+		
 	    String query = "insert into players values (" + "'" + lastName + "',"+ "'" + firstName + "'," + shirtNumber +  " )";
 //		String query = "insert into players values (?,?,?)"; 
 		Class.forName("com.mysql.cj.jdbc.Driver");                        // in detail: @ youtube "Class.forName" telusko
 		Connection con = DriverManager.getConnection(url, uname,pass);
 		PreparedStatement pst = con.prepareStatement(query);
 	//	ResultSet rs = pst.executeQuery(query);
+		
+	   //  String lastName = textField1.getText();
+	   //  String firsName = textField2.getText();
 	
 	//	pst.setString(1,wind1.getLastNameNW());  // index number is  referring to number of question mark (1 = first ? in String variable query)
 	//	pst.setString(2,wind1.getFirstNameNW());
 	//	pst.setString(3,wind1.getShirtNumberNW());
 	
 		
-	 //     Statement st = con.createStatement();   // use PreparedStatement instead for inserting/ changing of values!
-
+    //  Statement st = con.createStatement();   // use PreparedStatement instead for inserting/ changing of values!
+	//	ResultSet rs = pst.executeQuery(query);  
+		
+		
 		int count = pst.executeUpdate();    // count = number of rows that are effected                                     
 	//	ResultSet rs = pst.executeQuery(query); 
 		
@@ -49,7 +68,7 @@ public class JDBC {
 		}*/
 		
 		new GetList();
-		
+		 
 		pst.close();
 		con.close();
 		
@@ -70,12 +89,6 @@ public class JDBC {
 	}
 
 }
-
-
-
-
-
-
 
 
 
